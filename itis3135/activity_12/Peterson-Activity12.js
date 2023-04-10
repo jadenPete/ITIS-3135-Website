81cf6ab0f3a4c727f1d27183872c3e87f9686902
jadenPete <jadenpete@github.com>
2023-04-09 22:08:49 -0400

    Completed Activity 12

---
+++ itis3135/activity_12/Peterson-Activity12.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $("#slider").bxSlider({
+        auto: true,
+        minSlides: 2,
+        maxSlides: 2,
+        slideWidth: 250,
+        slideMargin: 10,
+        randomStart: true,
+        moveSlides: 1,
+        maxSlides: 1,
+        slideWidth: 500,
+        captions: true,
+        pause: 3000,
+        controls: true
+    });
+});
